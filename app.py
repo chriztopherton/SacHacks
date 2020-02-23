e33f46c8159bd9d4b39ea2605c8adb474edf61c4
Kevin Chu <kvnchu@ucdavis.edu>
2020-02-22 22:26:44 -0800

    Fancy header

--- app.py
+++ app.py
@@ -10,8 +10,10 @@ app = dash.Dash(__name__)
 
 
 # Load Data
-player_stats = pd.read_excel('2K Player Stats.xlsx')
-team_stats = pd.read_excel('2KL Team Stats.xlsx')
+team_url = 'https://drive.google.com/uc?export=download&id=1RoAG1mCkR9_O2EgVIZEpAJi6eVGtF-V0'
+player_url = 'https://drive.google.com/uc?export=download&id=119yYXWRKDelXqcv6CK2g0570iZKh0NWU'
+team_stats = pd.read_excel(team_url)
+player_stats = pd.read_excel(player_url)
 
 player_stats = player_stats.rename(columns = {"FG%":"FG_pct","FG3%":"FDG3_pct","FT%":"FT_pct","eFG%":"effi_pct","TS%":"Tru_shoot_pct"})
 team_stats = team_stats.rename(columns={"FG%":"FG_pct","3P%":"3P_pct","Opp_3P%":"Opp_3P_pct","Opp_FG%":"Opp_FG_pct"})
@@ -31,6 +33,7 @@ app.layout = html.Div(
                 dcc.Dropdown(
                     id = 'team1',
                     options = [{'label': i, 'value': i} for i in teams],
+                    value = 'Blazer5 Gaming'
                 ),
             ],
             style = {'width': '33%', 'display': 'inline-block', 'float': 'left'}
@@ -42,6 +45,7 @@ app.layout = html.Div(
                 dcc.Dropdown(
                     id = 'team2',
                     options = [{'label': i, 'value': i} for i in teams],
+                    value = '76ers GC'
                 ),
             ],
             style = {'width' : '34%', 'display': 'inline-block', 'float': 'center'}
@@ -52,7 +56,8 @@ app.layout = html.Div(
             [
                 dcc.Dropdown(
                     id = 'team_stat_category',
-                    options = [{'label': i, 'value': i} for i in team_stat_categories]
+                    options = [{'label': i, 'value': i} for i in team_stat_categories],
+                    value = 'Games_Played'
                 ),
             ],
             style = {'width': '33%', 'display': 'inline-block', 'float': 'right'}
@@ -65,31 +70,20 @@ app.layout = html.Div(
 
 @app.callback(
     Output('indicator-graphic', 'figure'),
-    [
-        Input('team1', 'value'),
-        Input('team2', 'value'),
-    ]
-)
+    [Input('team1', 'value'),
+     Input('team2', 'value'),
+     Input('team_stat_category', 'value')])
 
-@app.callback(
-    Output('indicator-graphic', 'figure'),
-    [
-        Input('team1', 'value'),
-        Input('team2', 'value'),
-        Input('team_stat_category', 'value')
-    ]
-)
-
-
-# def update_graph(xaxis_columnname, yaxis_columnname):
-#     return{
-#         'data': [
-#             dict(
-#                 # x = ,
-#                 y = team_stats[team_stats['']]
-#             )
-#         ]
-#     }
+def update_graph(team_1, team_2, stat):
+    return {
+        'data': [
+            {'x': [stat], 'y': team_stats[team_stats["Nickname"] == team_1][stat], 'type': 'bar', 'name': team_1},
+            {'x': [stat], 'y': team_stats[team_stats["Nickname"] == team_2][stat], 'type': 'bar', 'name': team_2},
+        ],
+        'layout': {
+            'title': (team_1 + ' vs. ' + team_2 + ' on ' + stat)
+        }
+    }
 
 
 if __name__ == '__main__':
